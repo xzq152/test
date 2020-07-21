3c1e1f1e987fbd04dde1883f20bfd0ec838511cb
xzq152 <778270578@qq.com>
2020-07-21 14:58:58 +0800

    提交信息

---
+++ docs/js/main.js
@@ -0,0 +1,88 @@
+$(function () {
+  'use strict';
+
+  var console = window.console || { log: function () {} };
+  var $images = $('.docs-pictures');
+  var $toggles = $('.docs-toggles');
+  var $buttons = $('.docs-buttons');
+  var options = {
+    // inline: true,
+    url: 'data-original',
+    show: function (e) {
+      console.log(e.type);
+    },
+    ready: function (e) {
+      console.log(e.type);
+    },
+    
+    shown: function (e) {
+      console.log(e.type);
+    },
+    hide: function (e) {
+      console.log(e.type);
+    },
+    hidden: function (e) {
+      console.log(e.type);
+    },
+    view: function (e) {
+      console.log(e.type);
+    },
+    viewed: function (e) {
+      console.log(e.type);
+    }
+  };
+
+  function toggleButtons(mode) {
+    if (/modal|inline|none/.test(mode)) {
+      $buttons
+        .find('button[data-enable]')
+          .prop('disabled', true)
+        .filter('[data-enable*="' + mode + '"]')
+          .prop('disabled', false);
+    }
+  }
+
+  $images.on({
+    ready:  function (e) {
+      console.log(e.type);
+    },
+    show:  function (e) {
+      console.log(e.type);
+    },
+    shown:  function (e) {
+      console.log(e.type);
+    },
+    hide:  function (e) {
+      console.log(e.type);
+    },
+    hidden: function (e) {
+      console.log(e.type);
+    },
+    view:  function (e) {
+      console.log(e.type);
+    },
+    viewed: function (e) {
+      console.log(e.type);
+    }
+  }).viewer(options);
+
+  toggleButtons(options.inline ? 'inline' : 'modal');
+
+  $toggles.on('change', 'input', function () {
+    var $input = $(this);
+    var name = $input.attr('name');
+
+    options[name] = name === 'inline' ? $input.data('value') : $input.prop('checked');
+    $images.viewer('destroy').viewer(options);
+    toggleButtons(options.inline ? 'inline' : 'modal');
+  });
+
+  $buttons.on('click', 'button', function () {
+    $('body').append('<img class="btn3" data-original="images/tibet-2.jpg" style="dispaly:none;"/>')
+    $('.btn3').viewer({ url: 'data-original',});
+    $('.btn3').viewer("show")
+   //console.log($('.btn4').viewer("show"))
+  });
+
+  $('[data-toggle="tooltip"]').tooltip();
+});
